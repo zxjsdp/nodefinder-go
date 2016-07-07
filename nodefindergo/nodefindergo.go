0e8463984d2d7e313386ab618ef81f1a39c23401
zxjsdp <zxjsdp@gmail.com>
2016-07-07 12:04:40 +0800

    Add config parser

--- nodefindergo/nodefindergo.go
+++ nodefindergo/nodefindergo.go
@@ -7,6 +7,19 @@ import (
 	"log"
 )
 
+const (
+	// Normal calibration info or clade label type
+	CALI_OR_CLADE_LABEL_TYPE = "cali_or_clade_label"
+	// Branch label type
+	BRANCH_LABEL_TYPE = "branchLabel"
+
+	// Blank name for branch label, which needs only one species name
+	BLANK_NAME_B = ""
+
+	DEFAULT_CALI_OR_CLADE_LABEL_DESCRIPTION = "Normal calibration or clade label."
+	DEFAULT_BRANCH_LABEL_DESCRIPTION = "Branch label description"
+)
+
 var (
 	NOT_TREE_NAME_SYMBOLS []rune = []rune {',', ';', ')', '"', '#', '$', '@', '>', '<'}
 	print = fmt.Println
@@ -97,9 +110,9 @@ func addSingleBranchLabel(rawTreeStr, nameA, branchLabel string) string {
 
 func MultipleCalibration(rawTreeStr string, calibrations []Calibration) string {
 	for index, cali := range calibrations {
-		if cali.caliType == "calibration" || cali.caliType == "cladeLabel" {
+		if cali.caliType == CALI_OR_CLADE_LABEL_TYPE {
 			rawTreeStr = singleCalibration(rawTreeStr, cali.nameA, cali.nameB, cali.caliInfo)
-		} else if cali.caliType == "branchLabel" {
+		} else if cali.caliType == BRANCH_LABEL_TYPE {
 			rawTreeStr = addSingleBranchLabel(rawTreeStr, cali.nameA, cali.caliInfo)
 		} else {
 			log.Panic("Invalid calibration!")
@@ -109,15 +122,45 @@ func MultipleCalibration(rawTreeStr string, calibrations []Calibration) string {
 	return strings.Replace(rawTreeStr, ",", ", ", -1)
 }
 
-func Test() {
-	var calibrations []Calibration
-	rawTreeStr := "((a,((b,c),(ddd,e))),(f,g));"
-
-	calibrations = []Calibration{
-		Calibration{0, "calibration", "b", "ddd", ">0.1<0.2", "First calibration"},
-		Calibration{1, "cladeLabel", "b", "c", ">0.03<0.05", "Second calibration"},
-		Calibration{2, "branchLabel", "f", "", "#3", "First branchLabel"},
+func parseConfig(configFileName string) []Calibration {
+	calibrations := []Calibration{};
+	lines := utils.ReadLines(configFileName)
+	for index, line := range lines {
+		line = strings.TrimSpace(line)
+		if line != "" && !strings.HasPrefix(line, "#") {
+			elements := strings.Split(line, ",")
+			if len(elements) == 3 {
+				elements = utils.CleanElements(elements)
+				currentCalibration := Calibration{index, CALI_OR_CLADE_LABEL_TYPE,
+					elements[0], elements[1], elements[2],
+					DEFAULT_CALI_OR_CLADE_LABEL_DESCRIPTION}
+				calibrations = append(calibrations, currentCalibration)
+			} else if len(elements) == 2 {
+				elements = utils.CleanElements(elements)
+				currentCalibration := Calibration{index, BRANCH_LABEL_TYPE,
+					elements[0], BLANK_NAME_B, elements[1],
+					DEFAULT_BRANCH_LABEL_DESCRIPTION}
+				calibrations = append(calibrations, currentCalibration)
+			} else {
+				log.Fatal(fmt.Sprintf("Invalid config line: %d", (index + 1)))
+			}
+		}
 	}
+	return calibrations
+}
 
+func Test() {
+	//var calibrations []Calibration
+	rawTreeStr := "((a,((b,c),(ddd,e))),(f,g));"
+	//
+	//calibrations = []Calibration{
+	//	Calibration{0, CALI_OR_CLADE_LABEL_TYPE, "b", "ddd", ">0.1<0.2", "First calibration"},
+	//	Calibration{1, CALI_OR_CLADE_LABEL_TYPE, "b", "c", ">0.03<0.05", "Second calibration"},
+	//	Calibration{2, BRANCH_LABEL_TYPE, "f", "", "#3", "First branchLabel"},
+	//}
+
+	//print(MultipleCalibration(rawTreeStr, calibrations))
+	calibrations := parseConfig(
+		"/Users/jin/go/src/github.com/zxjsdp/nodefinder-go/nodefindergo_tests/calibration.txt")
 	print(MultipleCalibration(rawTreeStr, calibrations))
 }
---
+++ utils/ioUtils.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"os"
+	"log"
+	"bufio"
+)
+
+func ReadLines(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return lines
+}
